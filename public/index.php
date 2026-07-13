4bbbd801089953cdcfb291eda72c65bdf2846999
Vetriveeran Gopalakrishnan <vgopalakrishnan@kinaxis.com>
2026-07-14 00:31:42 +0530

    Fix: strip /sundal/ subdir prefix from REQUEST_URI for shared hosting

--- public/index.php
+++ public/index.php
@@ -5,6 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Strip subdirectory prefix so routes match correctly on shared hosting
+// When deployed at /sundal/, REQUEST_URI contains /sundal/foo but routes expect /foo
+$subdir = '/sundal';
+if (isset($_SERVER['REQUEST_URI']) && str_starts_with($_SERVER['REQUEST_URI'], $subdir)) {
+    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($subdir)) ?: '/';
+}
+if (isset($_SERVER['PATH_INFO']) && str_starts_with($_SERVER['PATH_INFO'], $subdir)) {
+    $_SERVER['PATH_INFO'] = substr($_SERVER['PATH_INFO'], strlen($subdir)) ?: '/';
+}
+if (isset($_SERVER['PHP_SELF']) && str_starts_with($_SERVER['PHP_SELF'], $subdir)) {
+    $_SERVER['PHP_SELF'] = substr($_SERVER['PHP_SELF'], strlen($subdir)) ?: '/';
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
